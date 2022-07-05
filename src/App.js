01e0ec50dc82bb4ef5772f818856b4d705c0f54d
AKRAMSOUIDA <souidaa.akram@gmail.com>
2022-07-05 12:26:37 +0100

    checkpoint react1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import photo from './images/téléchargement.png';
+import logo from './images/gomycode.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gomycode' >
+        <img src={logo} alt="logo"></img>
+      </div>
+      <div className='forms'>
+        <div className="mb-3">
+          <label for="formFile" className="form-label">Upload your React final project</label>
+          <input className="form-control" type="file" id="formFile"></input>
+        </div>
+        <div className="mb-3">
+          <label for="formFileMultiple" className="form-label">Upload your components files</label>
+          <input className="form-control" type="file" id="formFileMultiple" multiple></input>
+        </div>
+        <div className="mb-3">
+          <label for="formFileSm" className="form-label">Upload your first React app </label>
+          <input className="form-control form-control-sm" id="formFileSm" type="file"></input>
+        </div>
+      </div>
+      <div className='image'>
+        <img src={photo} alt='githublogo'></img>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
